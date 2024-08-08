5930896596fdc9d7a62f42001a0bda5f394a7416
lee0502 <i63774071@gmail.com>
2024-08-08 10:33:59 +0900

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+
+        while (!input.equals("x")) {
+
+            System.out.print("Input Your Name: ");
+
+            String name = scanner.nextLine();
+            System.out.println("ToDo List 중 하나 선택 하세요!");
+            System.out.println("1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5.팀 멤버와의 1:1 면담");
+
+            System.out.print("title 번호: ");
+            String num = scanner.nextLine();
+            System.out.println("Status:  완료");
+            int num02 = Integer.parseInt(num);
+
+
+            System.out.print("종료 여부: ");
+            input = scanner.nextLine();
+
+
+
+        }
     }
 }
